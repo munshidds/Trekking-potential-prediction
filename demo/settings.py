b962af916d0faed94cf726afe3e469c44b11167a
unknown <munshid.ds@gamil.com>
2022-08-12 01:34:22 +0530

    completed

--- demo/settings.py
+++ demo/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = 
 
 # Default primary key field type
