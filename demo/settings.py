73b66bdc945b0b09a62cd1f8d0e8678332a5144f
unknown <munshid.ds@gamil.com>
2022-08-12 22:48:02 +0530

    initial commit

--- demo/settings.py
+++ demo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6z0ihya%dsst2z5rl^f1t70_b0@8cp*e6n6+!9jo_argm$ut68'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['deploy-demo-3197.herokuapp.com','127.0.0.1:8000/']
 
 
