8157d61878041fe0f62a1246aa6d9c5f3bea2ab2
unknown <munshid.ds@gamil.com>
2022-08-12 13:43:42 +0530

    completed

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('result/',views.result,name='result')
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,28 @@
 from ast import Return
 from django.shortcuts import render
+import sklearn
+import joblib
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html')
+
+def result(request):
+    cls=joblib.load('finalized_model.sav')
+    list=[]
+    list.append(request.GET['RI'])
+    list.append(request.GET['NA'])
+    list.append(request.GET['MG'])
+    list.append(request.GET['AI'])
+    list.append(request.GET['SI'])
+    list.append(request.GET['K'])
+    list.append(request.GET['BA'])
+    list.append(request.GET['CA'])
+    list.append(request.GET['FE'])
+
+    print(list)
+    ans= cls.predict([list])
+    
+    return render(request,'result.html',{'ans':ans,'List':list})
